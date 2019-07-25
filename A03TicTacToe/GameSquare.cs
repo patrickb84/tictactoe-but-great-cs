23afb0bd3425cf5b1fb001149a85a16a4c348c7f
[Patrick Bradshaw] <[patrick.j.bradshaw@gmail.com]>
2019-07-24 18:19:42 -0600

    Add tictactoe program files

---
+++ A03TicTacToe/GameSquare.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A03TicTacToe
+{
+    class GameSquare
+    {
+        public bool IsFilled { get; set; }
+        public char Token { get; set; }  //needed for win condition
+        public int Age { get; set; }
+
+
+        public void clearSquare()
+        {
+            IsFilled = false;
+            Token = ' ';
+        }
+
+    }
+}
